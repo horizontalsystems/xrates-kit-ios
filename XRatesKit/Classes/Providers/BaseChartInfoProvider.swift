3b6e56326c96335adca00d237fe709ea1ccbb9a7
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-03-05 20:14:40 +0600

    Add provider fallback for empty chart data response

--- XRatesKit/Classes/Providers/BaseChartInfoProvider.swift
+++ XRatesKit/Classes/Providers/BaseChartInfoProvider.swift
@@ -15,6 +15,13 @@ extension BaseChartInfoProvider: IChartPointProvider {
 
     func chartPointsSingle(key chartPointKey: ChartInfoKey) -> Single<[ChartPoint]> {
         cryptoCompareProvider.chartPointsSingle(key: chartPointKey)
+                .flatMap { [weak self] chartPoints in
+                    if self?.isChartDataValid(chartType: chartPointKey.chartType, chartPoints: chartPoints) ?? true {
+                        return Single.just(chartPoints)
+                    } else {
+                        return self?.coinGeckoProvider.chartPointsSingle(key: chartPointKey) ?? Single.just([])
+                    }
+                }
                 .catchError { [weak self] error in
                     if let error = error as? ProviderCoinsManager.ExternalIdError, error == .noMatchingCoinId {
                         return self?.coinGeckoProvider.chartPointsSingle(key: chartPointKey) ?? Single.just([])
@@ -22,6 +29,22 @@ extension BaseChartInfoProvider: IChartPointProvider {
 
                     return Single.error(error)
                 }
+    }
+
+    private func isChartDataValid(chartType: ChartType, chartPoints: [ChartPoint]) -> Bool {
+        guard let lastPoint = chartPoints.last else {
+            return false
+        }
+
+        if (Date().timeIntervalSince1970 - chartType.rangeInterval > lastPoint.timestamp) {
+            return false
+        }
+
+        if chartPoints.allSatisfy({ point in point.value == 0 }) {
+            return false
+        }
+
+        return true
     }
 
 }
